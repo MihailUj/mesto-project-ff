70d8cd6ee5d33df911a83ed75852dd43a95c6a59
Mihail Ujanin <MihailUj@yandex.ru>
2025-04-01 00:41:47 +0300

    Выполнение работы

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,22 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
+
+function addCard(card, deleteCard){
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__title').textContent = card.name;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+  placesList.append(cardElement);
+}
+
+function deleteCard(event){
+  event.target.parentElement.remove();
+}
+
+initialCards.forEach((item) => addCard(item,deleteCard));
